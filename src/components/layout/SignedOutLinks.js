65d46cbb91bd6c7fffe253b827295e9e36e08585
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-07-06 22:00:06 +0900

    Nav Links & CSS

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,11 +1,11 @@
-import  React from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const SignedOutLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to="/">Signup</NavLink></li>
-      <li><NavLink to="/">Login</NavLink></li>
+      <li><NavLink to="/signup">Signup</NavLink></li>
+      <li><NavLink to="/signin">Login</NavLink></li>
     </ul>
   );
 }
