fced448fabb6c2b5fc76e6a7c4fcca00c960fb48
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-07-11 16:25:26 +0900

    Logging Users Out

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -5,15 +5,20 @@ const initState = {
 const authReducer = (state = initState, action) => {
   switch (action.type) {
     case 'LOGIN_ERROR':
+      console.log('login error');
       return {
         ...state,
         authError: 'Login failed'
       }
     case 'LOGIN_SUCCESS':
+      console.log('login success');
       return {
         ...state,
         authError: null
       }
+    case 'SIGNOUT_SUCCESS':
+      console.log('logout success');
+      return state;
     default:
       return state;
   }
