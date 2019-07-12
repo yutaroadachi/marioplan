b3bcc254a303f48b6aa080272c9854e4505e912c
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-07-12 22:05:58 +0900

    Notification Component

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Notificataions = () => {
   return (
-    <div>
-      <p>Notificataions</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notificataions</span>
+          <ul className="notificataions">
+            <li>Notificataion</li>
+            <li>Notificataion</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
