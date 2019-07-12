2949a8c0f0b1719959078dc792176da7772947a9
yutaroadachi <ytr_adc72@yahoo.co.jp>
2019-07-12 23:22:42 +0900

    Cloud Function Auth Trigger

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,7 @@ const createNotification = notification => {
 exports.projectCreated = functions.firestore
   .document('projects/{projectId}')
   .onCreate(doc => {
+
     const project = doc.data();
     const notification = {
       content: 'Added a new project',
@@ -23,4 +24,21 @@ exports.projectCreated = functions.firestore
     }
 
     return createNotification(notification);
-  })
+  });
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+      });
+  });
